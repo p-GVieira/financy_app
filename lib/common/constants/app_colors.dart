30f81c62ed1615cb43e43615888d7896a2da41d5
Pedro Vieira <166433495+p-GVieira@users.noreply.github.com>
2025-11-20 14:38:28 -0300

    splash screen completion

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greenLightOne = Color(0xFF63B5AF);
+  static const Color greenLightTwo = Color(0xFF438883);
+  static const List<Color> greenGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883)
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/common/constants/app_text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bigText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 50.0, 
+    fontWeight: FontWeight.w700,
+  );
+}
